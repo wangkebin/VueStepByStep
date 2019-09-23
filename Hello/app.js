444a57a9b4e4a82421fa2a8168c200562c3724e0
Kebin Wang <wangkebin@hotmail.com>
2019-09-23 19:30:01 -0400

    Hello Vue
    Very first vue app

---
+++ Hello/app.js
@@ -0,0 +1,13 @@
+var kb1 = new Vue({
+    el: '#kb-app',
+    data: {
+        message: 'Hello Vue one!'
+    }
+});
+
+var kb2 = new Vue({
+    el: '#kb2-app',
+    data: {
+        message: 'Hello Vue from two!'
+    }
+});
